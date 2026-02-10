a988d8ca7f2792d249c0fc3582943e6b24bb3ce0
Rémi EECKHOUT <remieeckhout9@gmail.com>
2026-02-10 09:29:12 +0100

    feat: enhance user experience with updated templates for home, registration, and login pages; add featured racquets section and improve form layouts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RacquetRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -13,10 +14,13 @@ class HomeController extends AbstractController
      * @Route("/home", name="app_home")
      * @IsGranted("ROLE_USER")
      */
-    public function index(): Response
+    public function index(RacquetRepository $racquetRepository): Response
     {
+        // Get featured racquets (latest 6)
+        $featuredRacquets = $racquetRepository->findBy([], ['id' => 'DESC'], 6);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'featured_racquets' => $featuredRacquets,
         ]);
     }
 }
