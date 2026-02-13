38ba05e098ad8b0ee6073a6a640094a34a63c10b
Rémi EECKHOUT <remieeckhout9@gmail.com>
2026-02-13 15:11:55 +0100

    feat: add AccessDeniedListener to handle access denied exceptions and redirect users

---
+++ src/EventListener/AccessDeniedListener.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class AccessDeniedListener implements EventSubscriberInterface
+{
+    private $urlGenerator;
+    private $authorizationChecker;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // the priority must be greater than the Security HTTP
+            // ExceptionListener, to make sure it's called before
+            // the default exception listener
+            KernelEvents::EXCEPTION => ['onKernelException', 2],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        if (!$exception instanceof AccessDeniedException) {
+            return;
+        }
+
+        // ... perform some action (e.g. logging)
+
+        // optionally set the custom response
+        if (!$this->authorizationChecker->isGranted("ROLE_USER")) {
+            $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_login')));
+        }
+        $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_home')));
+
+        // or stop propagation (prevents the next exception listeners from being called)
+        //$event->stopPropagation();
+    }
+}
